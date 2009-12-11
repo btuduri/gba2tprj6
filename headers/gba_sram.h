d2b225f32e0b4440ed57c129aae3e50bd3b3754d
wesley.hilhorst <wesley.hilhorst@168de964-d2c4-11de-97ae-379497272b75>
2009-12-11 04:36:57 +0000

    Nieuwe startscreen, saven van coordinaten + loaden hiervan gaat 100% goed.

--- headers/gba_sram.h
+++ headers/gba_sram.h
@@ -11,38 +11,8 @@
 
 #define SRAM 0xe000000	//32k SRAM = 0x0e000000 - 0x0e00ffff
 
-void SaveInt(u16 offset, int value) 
-{
-	int i;
-	char string[7];
-	sprintf(string, "%d", value);
-	
-	for(i=0; i < 32768; i++) {
-	
-		if(string[i] == 0)	{
-			break;
-		}
-		
-		*(u8 *)(SRAM + offset + i) = string[i];
-		
-	}
-	
-}
-
-int LoadInt(u16 offset) {
-
-	int  i;
-	char string[7];
-	
-	for(i = 0; i != 7; i++) {
-	
-		string[i] = *(u8 *)(SRAM + offset + i);
-		
-	}
-	
-	return atoi(string);
-	
-}
-
+extern void eraseSRAM( int length );
+extern void SaveInt( u16 offset, int value );
+extern int LoadInt( u16 offset );
 
 #endif
--- menus/Startscreen.h
+++ menus/Startscreen.h
@@ -7,7 +7,7 @@
 //	+ bitmap not compressed
 //	Total size: 76800 = 76800
 //
-//	Time-stamp: 2009-12-10, 19:21:56
+//	Time-stamp: 2009-12-11, 05:28:02
 //	Exported by Cearn's GBA Image Transmogrifier, v0.8.3
 //	( http://www.coranac.com/projects/#grit )
 //
