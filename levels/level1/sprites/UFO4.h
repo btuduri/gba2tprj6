73735afdf9a4229656b778110773ce270c5587e5
sseegers <sseegers@168de964-d2c4-11de-97ae-379497272b75>
2009-12-25 12:50:25 +0000

    nieuw ufo sprites toegevoegd in totaal hebben we nu 6 enemies/ufo's. Zijn ook al in de makefile en in de AI toegevoegd dus gelijk bruikbaar :)

---
+++ levels/level1/sprites/UFO4.h
@@ -0,0 +1,28 @@
+
+//{{BLOCK(UFO4)
+
+//======================================================================
+//
+//	UFO4, 32x32@8, 
+//	+ palette 256 entries, not compressed
+//	+ 16 tiles not compressed
+//	Total size: 512 + 1024 = 1536
+//
+//	Time-stamp: 2009-12-25, 13:38:29
+//	Exported by Cearn's GBA Image Transmogrifier, v0.8.3
+//	( http://www.coranac.com/projects/#grit )
+//
+//======================================================================
+
+#ifndef GRIT_UFO4_H
+#define GRIT_UFO4_H
+
+#define UFO4TilesLen 1024
+extern const unsigned short UFO4Tiles[512];
+
+#define UFO4PalLen 512
+extern const unsigned short UFO4Pal[256];
+
+#endif // GRIT_UFO4_H
+
+//}}BLOCK(UFO4)
---
+++ levels/level1/sprites/UFO6.h
@@ -0,0 +1,28 @@
+
+//{{BLOCK(UFO6)
+
+//======================================================================
+//
+//	UFO6, 32x32@8, 
+//	+ palette 256 entries, not compressed
+//	+ 16 tiles not compressed
+//	Total size: 512 + 1024 = 1536
+//
+//	Time-stamp: 2009-12-25, 13:36:21
+//	Exported by Cearn's GBA Image Transmogrifier, v0.8.3
+//	( http://www.coranac.com/projects/#grit )
+//
+//======================================================================
+
+#ifndef GRIT_UFO6_H
+#define GRIT_UFO6_H
+
+#define UFO6TilesLen 1024
+extern const unsigned short UFO6Tiles[512];
+
+#define UFO6PalLen 512
+extern const unsigned short UFO6Pal[256];
+
+#endif // GRIT_UFO6_H
+
+//}}BLOCK(UFO6)
