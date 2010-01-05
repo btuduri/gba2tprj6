e57e94d7fa81fc65a5f61abbd4e7dbcb02407355
wesley.hilhorst <wesley.hilhorst@168de964-d2c4-11de-97ae-379497272b75>
2010-01-05 14:21:33 +0000

    Gameover scherm voor Sebastiaan

---
+++ menus/Gameover.h
@@ -0,0 +1,24 @@
+
+//{{BLOCK(Gameover)
+
+//======================================================================
+//
+//	Gameover, 240x160@16, 
+//	+ bitmap not compressed
+//	Total size: 76800 = 76800
+//
+//	Time-stamp: 2010-01-05, 15:20:13
+//	Exported by Cearn's GBA Image Transmogrifier, v0.8.3
+//	( http://www.coranac.com/projects/#grit )
+//
+//======================================================================
+
+#ifndef GRIT_GAMEOVER_H
+#define GRIT_GAMEOVER_H
+
+#define GameoverBitmapLen 76800
+extern const unsigned short GameoverBitmap[38400];
+
+#endif // GRIT_GAMEOVER_H
+
+//}}BLOCK(Gameover)
