fcf827a5ab99c855917b2b9a21ffe95684f9a693
sseegers <sseegers@168de964-d2c4-11de-97ae-379497272b75>
2010-01-05 15:13:15 +0000

    Game over scherm gemaakt, highscores worden als het goed is opgeslagen als je gameover gaat. Nog niet kunnen testen omdat de highscores nog niet getoond worden.

--- headers/menus.h
+++ headers/menus.h
@@ -2,8 +2,8 @@
  * Keeper of the necessary menus header/sources.
  *
  * @date	11/12/09
- * @author  Wesley Hilhorst
- * @email	wesley.hilhorst@gmail.com
+ * @author  Wesley Hilhorst, Sebastiaan Seegers	
+ * @email	wesley.hilhorst@gmail.com, Sseegers@gmail.com
  */
 
 #ifndef MENUS_HEADER
@@ -11,5 +11,7 @@
 
 #include "../menus/startscreen.h" 	// Startscreen
 #include "../menus/Pausescreen.h" 	// Pausescreen
+#include "../menus/Gameover.h"		// Gameoverscreen
+
 
 #endif
---
+++ levels/level1/sprites/powerup1.h
@@ -0,0 +1,28 @@
+
+//{{BLOCK(powerup1)
+
+//======================================================================
+//
+//	powerup1, 32x32@8, 
+//	+ palette 256 entries, not compressed
+//	+ 16 tiles not compressed
+//	Total size: 512 + 1024 = 1536
+//
+//	Time-stamp: 2010-01-02, 10:56:28
+//	Exported by Cearn's GBA Image Transmogrifier, v0.8.3
+//	( http://www.coranac.com/projects/#grit )
+//
+//======================================================================
+
+#ifndef GRIT_POWERUP1_H
+#define GRIT_POWERUP1_H
+
+#define powerup1TilesLen 1024
+extern const unsigned short powerup1Tiles[512];
+
+#define powerup1PalLen 512
+extern const unsigned short powerup1Pal[256];
+
+#endif // GRIT_POWERUP1_H
+
+//}}BLOCK(powerup1)
