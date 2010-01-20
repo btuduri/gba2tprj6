28e19accf46e860dc3661d28cc8aabedb1e04884
sseegers <sseegers@168de964-d2c4-11de-97ae-379497272b75>
2010-01-20 17:02:55 +0000

    extra powerup toegevoegd, powerups werken bijna allemaal alleen er zit nog een bug in!

---
+++ trunk/levels/level1/sprites/powerup3.h
@@ -0,0 +1,28 @@
+
+//{{BLOCK(powerup3)
+
+//======================================================================
+//
+//	powerup3, 32x32@8, 
+//	+ palette 256 entries, not compressed
+//	+ 16 tiles not compressed
+//	Total size: 512 + 1024 = 1536
+//
+//	Time-stamp: 2010-01-20, 17:29:14
+//	Exported by Cearn's GBA Image Transmogrifier, v0.8.3
+//	( http://www.coranac.com/projects/#grit )
+//
+//======================================================================
+
+#ifndef GRIT_POWERUP3_H
+#define GRIT_POWERUP3_H
+
+#define powerup3TilesLen 1024
+extern const unsigned short powerup3Tiles[512];
+
+#define powerup3PalLen 512
+extern const unsigned short powerup3Pal[256];
+
+#endif // GRIT_POWERUP3_H
+
+//}}BLOCK(powerup3)
